effebf2f0c63b74933c754db5f7fef05138a8603
Natars <pasero.romain@gmail.com>
2019-12-01 10:25:43 +0000

    Positionnement des enregistrements dans un tableau à plusieurs colonnes

--- index.php
+++ index.php
@@ -16,18 +16,40 @@
 						//Listing des fichiers présents dans le dossier "camera"
 						$listeEnregistrement = scandir('./camera');
 
-						//Affichage de la liste des enregistrements
-						echo '<table>';
+						//Affichage de la liste des enregistrements dans un tableau
+						echo '<table><tr>';
+						$nombreColonneTableau = 2;
+
+						$nombreElementTableau = count($listeEnregistrement); //On compte le nombre d'éléments du tableau
+						$nombreElementTableau -= 2; //On décompte le '.' et le '..' de la liste des éléments
+
+						$nombreLigneTableau = intdiv($nombreElementTableau, $nombreColonneTableau);
+						if(($nombreElementTableau % $nombreColonneTableau) > 0) //On détermine le nombre de ligne du tableau
+						{
+							$nombreLigneTableau++; //Si besoin, on rajoute une ligne pour mettre les éléments restants
+						}
+
+						$numeroColonne = 0; //Numéro de colonne où va être positionné l'élément
 						foreach($listeEnregistrement as $enregistrement)
 						{
 							if($enregistrement != '.' && $enregistrement != '..')
 							{
-								echo '<tr>';
+								if($numeroColonne >= $nombreColonneTableau)
+								{ //Si toutes les colonnes d'une ligne sont remplies, on passe à la suivante
+									echo "</tr><tr>";
+									$numeroColonne = 0;
+								}
+
+								echo '<td>';
 								echo "<p>$enregistrement</p>";
-								echo '</tr>';
+								echo '</td>';
+								$numeroColonne++; //On passe à la colonne suivante
 							}
 						}
-						echo '</table>';
+						echo '</tr></table>';
+
+						echo "quotient: ". intdiv($nombreElementTableau, $nombreColonneTableau);
+						echo " reste: ". $nombreElementTableau % $nombreColonneTableau;
 					?>
 				</div>
 			</div>
