c115ae7ca0e652368a78b84327a16b0ff8a18395
Natars <pasero.romain@gmail.com>
2019-12-04 21:09:51 +0100

    Ajout d'un mémo au niveau de la ligne de débogage Définition du fuseau horaire à utiliser pour toutes les fonctions "date"

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
-<?php error_reporting(E_ALL); ini_set("display_errors", 1); ?>
+<?php error_reporting(E_ALL); ini_set("display_errors", 1); #A supprimer dès que la page fonctionne ?>
+<?php date_default_timezone_set('Europe/Paris'); ?>
 <!DOCTYPE html>
 <html>
     <?php include("include/header.php"); ?>
