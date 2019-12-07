778d16be1e7032cf2e7de5434236da20d265c6ad
Natars <pasero.romain@gmail.com>
2019-12-07 12:23:54 +0100

    Page vierge, à copier à chaque ajout de page au site web

---
+++ modele.php
@@ -0,0 +1,24 @@
+<?php error_reporting(E_ALL); ini_set("display_errors", 1); #A supprimer dès que la page fonctionne ?>
+<?php date_default_timezone_set('Europe/Paris'); ?>
+<!DOCTYPE html>
+<html>
+    <?php include("include/header.php"); ?>
+
+    <body>
+	<div class="main">
+		<div class="container">
+			<div class="gfx"><a href="index.php"><span></span></a></div>
+			<?php include("include/menu.php"); ?>
+
+			<div class="content">
+				<div class="item">
+
+				<!-- CONTENU DE LA PAGE WEB -->
+
+				</div>
+			</div>
+			<?php include("include/footer.php") ?>
+		</div>
+	</div>
+    </body>
+</html>
