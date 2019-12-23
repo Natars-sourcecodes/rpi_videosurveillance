dea4614d1a8517cd5817ef64a20ddf475d823cf8
Natars <pasero.romain@gmail.com>
2019-12-23 19:17:05 +0100

    Mise en place du lien vers une page phpinfo.php et phpmyadmin afin de faciliter le développement du site

--- include/menu.php
+++ include/menu.php
@@ -1,2 +1,6 @@
 <div class="menu">
+	<a href="index.php">Liste des enregistrements</a>
+	<a href="parametre.php">Paramétrage de la caméra</a>
+	<a href="phpinfo.php">Consulter le phpinfo</a>
+	<a href="phpmyadmin">Accès à phpMyAdmin</a>
 </div>
