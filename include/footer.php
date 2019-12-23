aadcb788dc69c34374e18f0d7d110b9ddc1cd015
Natars <pasero.romain@gmail.com>
2019-12-23 19:16:23 +0100

    Mise à jour de l'année

--- include/footer.php
+++ include/footer.php
@@ -1,4 +1,4 @@
 <div class="footer">
-	<span class="left">&copy; 2018 - Site créé par Romain.</span>
+	<span class="left">&copy; 2019 - Site créé par Romain.</span>
 	<div class="clearer"><span></span></div>
 </div>
