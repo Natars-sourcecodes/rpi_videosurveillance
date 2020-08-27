39a9050e91fab4f34c456e61993ce22c652bf059
natars-sourcecodes <pasero.romain@gmail.com>
2020-08-27 22:32:33 +0200

    Mise en place d'un système de boucle permettant de prendre 10 photos (peut être modifié au début du fichier). L'intervalle entre deux photos est de quelques secondes (dépend du temps que met "raspistill" à générer l'image)
    Mise à jour de la commande raspistill:
     - Remplacements de "-vf" et "-hf" par "--rotate 270": retourne l'image de 270° dans le sens horaire.
     - Modification du chemin de l'image générée: elle est déposée dans le répertoire "photo" présent au niveau du script

--- script/camera/prendrePhoto.sh
+++ script/camera/prendrePhoto.sh
@@ -1,1 +1,11 @@
-raspistill -vf -hf -ex auto -o /home/pi/camera/photo/$(date +"%Y-%m-%d_%H:%M:%S").jpg >> /dev/null
+#!/bin/bash
+
+nombrePhotoPrise=0
+nombrePhotoAPrendre=10
+
+until [ $nombrePhotoPrise -eq $nombrePhotoAPrendre ]
+do
+	raspistill --rotation 270 -ex auto -o /var/www/html/script/camera/photo/$(date +"%Y-%m-%d_%H:%M:%S").jpg >> /dev/null
+	nombrePhotoPrise=$(($nombrePhotoPrise+1))
+	#echo "Photo numero $nombrePhotoPrise"
+done
