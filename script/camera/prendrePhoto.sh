f2a371946ea7fb7980e486788eae827a042f5817
natars-sourcecodes <pasero.romain@gmail.com>
2020-08-29 20:35:17 +0200

    Ajout de la prise de photo pour le bot "telegram":  - Au lancement du script, si l'argument "telegram" est reçu, une photo nommée "telegram.jpg" est générée  - La photo est placée dans le dossier "photo".
    Suppression du "/" dans le chemin préenregistré afin de gagner en lisiblité lors de son utilisation.

--- script/camera/prendrePhoto.sh
+++ script/camera/prendrePhoto.sh
@@ -2,27 +2,24 @@
 
 nombrePhotoPrise=0
 nombrePhotoAPrendre=10
-dossierSauvegardePhoto='/var/www/html/script/camera/photo/'
+dossierSauvegardePhoto='/var/www/html/script/camera/photo'
 
-#La commande de base à exécuter
-commandePhoto="raspistill --rotation 270 -ex auto"
+commandePhoto="raspistill --rotation 270 -ex auto "
 
 if [[ $# -eq 0 ]]
 then
 	until [ $nombrePhotoPrise -eq $nombrePhotoAPrendre ]
 	do
-		#On génère le nom de la photo puis on l'ajoute à la commande
 		nomPhoto="$(date +%Y-%m-%d_%H-%M-%S).jpg"
-		commandePhoto+=" -o $dossierSauvegardePhoto$nomPhoto"
-
-		#Une fois terminée, on exécute la commande
+		commandePhoto+="-o $dossierSauvegardePhoto/$nomPhoto"
 		$($commandePhoto)
-
-		#On comptabilise la photo prise
 		nombrePhotoPrise=$(($nombrePhotoPrise+1))
 		#echo "Photo numero $nombrePhotoPrise"
 	done
-elif [[ $# -eq 1 ]]
+elif [[ $# -eq 1 && $1 == "telegram" ]]
 then
-	sleep 1
+	#Si une photo est demandée par le bot Telegram, on l'identifie pour qu'il la trouve
+	nomPhoto="telegram.jpg"
+	commandePhoto+="-o $dossierSauvegardePhoto/$nomPhoto"
+	$($commandePhoto)
 fi
