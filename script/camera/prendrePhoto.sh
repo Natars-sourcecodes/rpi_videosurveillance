6bbd2f859e5237fefb31444b52d5a92b40e16fc1
natars-sourcecodes <pasero.romain@gmail.com>
2020-08-29 19:43:37 +0200

    Mise en place d'un système de construction de commande: 	Permet de construire une commande en fonction de l'évolution du script. Celle-ci est ensuite exécutée.
    Mise en place d'un test sur la présence ou non d'arguments en vue de la prochaine évolution du script.
    	Son lancement "normal" se fait sans passer d'argument au script

--- script/camera/prendrePhoto.sh
+++ script/camera/prendrePhoto.sh
@@ -2,10 +2,27 @@
 
 nombrePhotoPrise=0
 nombrePhotoAPrendre=10
+dossierSauvegardePhoto='/var/www/html/script/camera/photo/'
 
-until [ $nombrePhotoPrise -eq $nombrePhotoAPrendre ]
-do
-	raspistill --rotation 270 -ex auto -o /var/www/html/script/camera/photo/$(date +"%Y-%m-%d_%H:%M:%S").jpg >> /dev/null
-	nombrePhotoPrise=$(($nombrePhotoPrise+1))
-	#echo "Photo numero $nombrePhotoPrise"
-done
+#La commande de base à exécuter
+commandePhoto="raspistill --rotation 270 -ex auto"
+
+if [[ $# -eq 0 ]]
+then
+	until [ $nombrePhotoPrise -eq $nombrePhotoAPrendre ]
+	do
+		#On génère le nom de la photo puis on l'ajoute à la commande
+		nomPhoto="$(date +%Y-%m-%d_%H-%M-%S).jpg"
+		commandePhoto+=" -o $dossierSauvegardePhoto$nomPhoto"
+
+		#Une fois terminée, on exécute la commande
+		$($commandePhoto)
+
+		#On comptabilise la photo prise
+		nombrePhotoPrise=$(($nombrePhotoPrise+1))
+		#echo "Photo numero $nombrePhotoPrise"
+	done
+elif [[ $# -eq 1 ]]
+then
+	sleep 1
+fi
