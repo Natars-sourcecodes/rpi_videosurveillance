6b936f94d8143f1715f7ee89e8bebe3c21bcd10a
natars-sourcecodes <pasero.romain@gmail.com>
2020-09-09 14:55:25 +0200

    Suppressions des deux commandes de test "getChat()" et "getMe()"
    Modification de la fonction journaliser_messages()
     - Modification des données enregistrées dans le fichier de log.
       L'ID expéditeur et destinataire a été ajouté, "expéditeur_autorise" a été supprimé
    
     - Vérifier la pertinence de ces modifications pour report dans le script "botTelegram.py"
    
    L'utilisation de l'ID permet d'identifier un compte quelque soit son nom d'utilisateur (permet de mieux parer les spams)

--- script/telegram/envoyerMessageTelegram.py
+++ script/telegram/envoyerMessageTelegram.py
@@ -13,19 +13,23 @@ def cleAPIbot():
 #Fonction chargée de l'envoi de message à un utilisateur
 def envoyer_message(id_utilisateur, message):
 	global bot
+	infoExpediteur = bot.getMe()
+	infoDestinataire = bot.getChat(id_utilisateur)
+
 	bot.sendMessage(id_utilisateur, message)
+	journaliser_message(datetime.now(),infoExpediteur.username,infoExpediteur.id,infoDestinataire.username,infoDestinataire.id,message)
 
 #Fonction servant à enregistrer les activités du chat dans un fichier de log
-def journaliser_message(date_envoi,expediteur,expediteur_valide,destinataire,id_destinataire,message):
+def journaliser_message(date_envoi,expediteur,id_expediteur,destinataire,id_destinataire,message):
 	nomFichierLOG = 'chat.log'
 
 	#On prépare la ligne qui sera enregistrée dans le fichier de log
-	entree_journal_log = '"%s","None","%s","%s","%s","%s"\n' % (date_envoi,expediteur,destinataire,message,expediteur_valide)
+	entree_journal_log = '"%s","None","%s","%s","%s","%s","%s"\n' % (date_envoi,expediteur,id_expediteur,destinataire,id_destinataire,message)
 
 	#Si le fichier de log n'existe pas, on le créé et on inscrit les titres de colonnes
 	if os.path.exists(nomFichierLOG) == False:
 		with open(nomFichierLOG,'w') as fichierLOG:
-			fichierLOG.write('"Date d\'envoi","Date de réception","Expéditeur","Destinataire","Message","Expéditeur autorisé"\n')
+			fichierLOG.write('"Date d\'envoi","Date de réception","Expéditeur","ID expéditeur","Destinataire","ID destinataire","Message"\n')
 
 	#Écriture de l'entrée dans le journal
 	with open(nomFichierLOG,'a') as fichierLOG:
@@ -37,8 +41,4 @@ ID_destinataire = "something"
 #Initialisation du bot avec sa clé API
 bot = telegram.Bot(token=cleAPIbot())
 
-envoyer_message(ID_destinataire,"test")
-
-#Commandes de test
-print(bot.getChat(ID_destinataire).username)
-print(bot.getMe())
+envoyer_message(id_destinataire,"test")
