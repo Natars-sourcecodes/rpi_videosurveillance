2647bda8dd3f1d2daf4e6cde8158f0dc4bf1f1e0
natars-sourcecodes <pasero.romain@gmail.com>
2020-09-24 11:00:03 +0200

    Réduction du nombre de photos prises par le script (10 -> 5)
    Dans la commande "raspistill" de base, ajout sur la photo de la date et l'heure (format américain)
    
    Correction d'une erreur dans la boucle au niveau de la génération de la commande à exécuter:
     * La commande à exécuter est placé dans la variable "commandePhotoAExecuter"
     * Permet d'éviter de modifier la commande de base et de renconter une erreur à chaque nouveau tour de boucle
    
    Mise en place d'un système de listing des photos réalisés afin d'être utilisés par le script "envoyerMessageTelegram.py" via "videosurveillance.py"

--- script/camera/prendrePhoto.sh
+++ script/camera/prendrePhoto.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
 nombrePhotoPrise=0
-nombrePhotoAPrendre=10
+nombrePhotoAPrendre=5
 dossierSauvegardePhoto='/var/www/html/script/camera/photo'
 
-commandePhoto="raspistill --rotation 270 -ex auto "
+commandePhoto="raspistill --rotation 270 -ex auto -ae 128,0xff,0x0808000 -a 12 "
 
 if [[ $# -eq 0 ]]
 then
+	listePhoto=""
 	until [ $nombrePhotoPrise -eq $nombrePhotoAPrendre ]
 	do
 		nomPhoto="$(date +%Y-%m-%d_%H-%M-%S).jpg"
-		commandePhoto+="-o $dossierSauvegardePhoto/$nomPhoto"
-		$($commandePhoto)
+		commandePhotoAExecuter+="$commandePhoto -o $dossierSauvegardePhoto/$nomPhoto"
+		$($commandePhotoAExecuter)
+
 		nombrePhotoPrise=$(($nombrePhotoPrise+1))
-		#echo "Photo numero $nombrePhotoPrise"
+		listePhoto+="$dossierSauvegardePhoto/$nomPhoto "
 	done
+
+	echo $listePhoto
+
 elif [[ $# -eq 1 && $1 == "telegram" ]]
 then
 	#Si une photo est demandée par le bot Telegram, on l'identifie pour qu'il la trouve
