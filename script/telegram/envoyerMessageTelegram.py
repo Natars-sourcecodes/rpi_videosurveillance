a0d3ae8c7baf9966669697cd69f9315d049fa56f
natars-sourcecodes <pasero.romain@gmail.com>
2020-09-29 12:06:48 +0200

    Mise en place du module "fonctionCommune":  - Déplacement de "cleAPIbot" dedans (renommé en "cle_api_bot")  - Déplacement de "journaliser_message" dedans (renommé en "journaliser_message_chat")  - Mise à jour de toutes les lignes de codes utilisant ces fonctions
    Insertion de la ligne d'initialisation dans un blox try except:
     - En cas d'erreur: affichage d'un message d'erreur et fin du script

--- script/telegram/envoyerMessageTelegram.py
+++ script/telegram/envoyerMessageTelegram.py
@@ -3,15 +3,11 @@
 #Python Telegram Bot
 #Documentation: https://python-telegram-bot.readthedocs.io
 
+import fonctionCommune
 import sys
 import os, os.path
 from datetime import datetime
 import telegram
-
-def cleAPIbot():
-	with open('/var/www/html/script/telegram/cleAPI.txt', 'r') as fichierCleAPI:
-		cleAPI = fichierCleAPI.read()
-	return cleAPI
 
 #Fonction chargée de l'envoi de message à un utilisateur
 def envoyer_message(id_utilisateur, message):
@@ -20,7 +16,7 @@ def envoyer_message(id_utilisateur, message):
 	infoDestinataire = bot.getChat(id_utilisateur)
 
 	bot.sendMessage(id_utilisateur, message)
-	journaliser_message(datetime.now(),infoExpediteur.username,infoExpediteur.id,infoDestinataire.username,infoDestinataire.id,message)
+	fonctionCommune.journaliser_message_chat(datetime.now(),None,infoExpediteur.username,infoExpediteur.id,infoDestinataire.username,infoDestinataire.id,message)
 
 def envoyer_photo(id_utilisateur, chemin_vers_la_photo):
 	global bot
@@ -30,29 +26,18 @@ def envoyer_photo(id_utilisateur, chemin_vers_la_photo):
 	bot.send_photo(id_utilisateur, open(chemin_vers_la_photo, 'rb'))
 
 	message = "[photo: %s]" % chemin_vers_la_photo
-	journaliser_message(datetime.now(),infoExpediteur.username,infoExpediteur.id,infoDestinataire.username,infoDestinataire.id,message)
-
-#Fonction servant à enregistrer les activités du chat dans un fichier de log
-def journaliser_message(date_envoi,expediteur,id_expediteur,destinataire,id_destinataire,message):
-	nomFichierLOG = '/var/www/html/script/telegram/chat.log'
-
-	#On prépare la ligne qui sera enregistrée dans le fichier de log
-	entree_journal_log = '"%s","None","%s","%s","%s","%s","%s"\n' % (date_envoi,expediteur,id_expediteur,destinataire,id_destinataire,message)
-
-	#Si le fichier de log n'existe pas, on le créé et on inscrit les titres de colonnes
-	if os.path.exists(nomFichierLOG) == False:
-		with open(nomFichierLOG,'w') as fichierLOG:
-			fichierLOG.write('"Date d\'envoi","Date de réception","Expéditeur","ID expéditeur","Destinataire","ID destinataire","Message"\n')
-
-	#Écriture de l'entrée dans le journal
-	with open(nomFichierLOG,'a') as fichierLOG:
-		fichierLOG.write(entree_journal_log)
+	fonctionCommune.journaliser_message_chat(datetime.now(),None,infoExpediteur.username,infoExpediteur.id,infoDestinataire.username,infoDestinataire.id,message)
 
 #l'ID de mon compte Telegram (peut être utilisé comme 'chat_id' pour converser en chat privé avec un utilisateur)
-id_destinataire = "Something"
+id_destinataire = "something"
 
-#Initialisation du bot avec sa clé API
-bot = telegram.Bot(token=cleAPIbot())
+try:
+	#Initialisation du bot avec sa clé API
+	bot = telegram.Bot(token=fonctionCommune.cle_api_bot())
+
+except:
+	print("Erreur de lancement du script")
+	exit()
 
 #On contrôle les arguments reçus et on adopte le comportement adapté
 if len(sys.argv) > 2: #Contrôle du nombre d'argument reçus (arg. n°1: titre du script, arg. n°2: type d'action à réaliser, arg. n°3,4,...: paramètres nécessaire à la fonction )
