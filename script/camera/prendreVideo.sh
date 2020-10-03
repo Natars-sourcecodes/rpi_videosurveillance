0a058b98c754d4c6c2770fe4bf21f5040ea866d7
Natars-sourcecodes <pasero.romain@gmail.com>
2020-10-03 14:52:59 +0200

    Allongement de la durée d'enregistrement à 2min (mis à 5sec durant les tests)
    Rotation de l'image afin que l'image soit à l'endroit (par rapport au positionnement de la caméra chez moi)

--- script/camera/prendreVideo.sh
+++ script/camera/prendreVideo.sh
@@ -5,7 +5,7 @@ dateHeure=$(date +"%Y-%m-%d_%H:%M:%S")
 enregistrement="$dateHeure.mp4"
 
 #Enregistrement de la vidéo
-raspivid -a 4 -a "%d/%m/%Y %X" -drc med -ex auto -t 5000 -w 1640 -h 1232 -fps 25 -vf -hf -o camera.h264 >> /dev/null
+raspivid -a 4 -a "%d/%m/%Y %X" -drc med -ex auto -t 120000 -w 1640 -h 1232 -fps 25 --rotation 270 -o camera.h264 >> /dev/null
 
 #Conversion en MP4 et déplacement dans le dossier adéquat
 MP4Box -add camera.h264 $enregistrement >> /dev/null
