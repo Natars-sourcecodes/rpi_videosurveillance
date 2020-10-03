6559905f33788daf0ab3573f1f976d2bffcebad7
Natars-sourcecodes <pasero.romain@gmail.com>
2020-10-03 14:08:10 +0200

    Ajout de la fonction d'envoi de vidéo sur Telegram
    Ajout d'un commentaire manquant: descriptif de la fonction envoyer_photo()
    
    A faire:
     - Régler le problème d'import du module fonctionCommune dès que ce script est lancé d'ailleur que le dossier "telegram"

--- script/telegram/envoyerMessageTelegram.py
+++ script/telegram/envoyerMessageTelegram.py
@@ -18,6 +18,7 @@ def envoyer_message(id_utilisateur, message):
 	bot.sendMessage(id_utilisateur, message)
 	fonctionCommune.journaliser_message_chat(datetime.now(),None,infoExpediteur.username,infoExpediteur.id,infoDestinataire.username,infoDestinataire.id,message)
 
+#Fonction chargée de l'envoi d'une photo à un utilisateur
 def envoyer_photo(id_utilisateur, chemin_vers_la_photo):
 	global bot
 	infoExpediteur = bot.getMe()
@@ -28,10 +29,24 @@ def envoyer_photo(id_utilisateur, chemin_vers_la_photo):
 	message = "[photo: %s]" % chemin_vers_la_photo
 	fonctionCommune.journaliser_message_chat(datetime.now(),None,infoExpediteur.username,infoExpediteur.id,infoDestinataire.username,infoDestinataire.id,message)
 
+#Fonction chargée de l'envoi d'une vidéo à un utilisateur
+def envoyer_video(id_utilisateur, chemin_vers_la_video):
+	global bot
+	infoExpediteur = bot.getMe()
+	infoDestinataire = bot.getChat(id_utilisateur)
+
+	bot.send_video(id_utilisateur, open(chemin_vers_la_video, 'rb'))
+
+	message = "[video: %s]" % chemin_vers_la_video
+	fonctionCommune.journaliser_message_chat(datetime.now(),None,infoExpediteur.username,infoExpediteur.id,infoDestinataire.username,infoDestinataire.id,message)
+
 #l'ID de mon compte Telegram (peut être utilisé comme 'chat_id' pour converser en chat privé avec un utilisateur)
 id_destinataire = "something"
 
 try:
+	print("Démarrage")
+	print(fonctionCommune.cle_api_bot())
+
 	#Initialisation du bot avec sa clé API
 	bot = telegram.Bot(token=fonctionCommune.cle_api_bot())
 
@@ -54,3 +69,10 @@ if len(sys.argv) > 2: #Contrôle du nombre d'argument reçus (arg. n°1: titre d
 		#Il ne nous reste alors que la liste des photos à envoyer, on boucle alors dessus
 		for photo in sys.argv:
 			envoyer_photo(id_destinataire,str(photo))
+
+	#Vidéo (une ou plusieurs)
+	if sys.argv[1] == "video":
+		del sys.argv[0:2]
+
+		for video in sys.argv:
+			envoyer_video(id_destinataire,str(video))
