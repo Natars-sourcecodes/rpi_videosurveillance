2aea83d953fc2ab916029c74429fcc4e791ae82a
Natars-sourcecodes <pasero.romain@gmail.com>
2020-12-16 14:26:34 +0100

    Ce script est chargé de la gestion des messages du chat.
    Il comprends 4 fonctions:
     - envoyer_texte()
        Cette fonction permet d'envoyer un message texte sur le chat.
        Elle prends deux arguments:
          - update: variable contenant l'ensemble des informations concernant la dernière commande reçue par le bot
          - texte_a_envoyer: le texte que le bot doit envoyer sur le chat.
    
        Le bot commence par envoyer le message. Il extrait ensuite son pseudo et son ID des variables d'environnements afin de pouvoir procéder à la jouralisation.
        Enfin, il appelle la fonction journaliser_message() avec les arguments nécessaires afin d'enregistrer ce message dans le journal.
    
     - date_utc_vers_local()
        Cette fonction permet simplement de convertir une date et heure UTC en date et heure locale.
        Elle supprime également les millisecondes de l'heure.
        Le seul argument nécessaire est la date/heure à convertir, elle renvoie la date/heure convertie.
    
     - journaliser_message_recu() lors de la réception d'un message sur le chat, permet d'extraire les information nécessaire et les transmet à la fonction suivante
        Lorsqu'une commande est reçue, toutes les informations la concernant sont stockés dans une variable de type Update.
        Cette fonction permet simplement d'y extraire les informations nécessaire afin de procéder à la journalisation de la commande reçue.
        Une fois fait, elle exécute la fonction "journaliser_message()"
        Le seul argument nécessaire est la variable de type Update.
    
     - journaliser_message()
        Cette fonction permet d'ajouter une nouvelle entrée dans le journal de chat.
        Elle nécessite 7 arguments:
          - date_envoi: date/heure à laquelle le message a été envoyé
          - date_reception: date/heure à laquelle le message à été reçu
          - expediteur: pseudonyme de l'expéditeur (seulement à titre indicatif)
          - id_expediteur: numéro d'identification du compte Telegram de l'expéditeur
          - destinataire: pseudonyme du destinataire (seulement à titre indicatif)
          - id_expediteur: numéro d'identification du compte Telegram du destinataire
          - message: le contenu du message à sauvegarder

---
+++ script/telegram/message.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python
+# coding: utf-8
+#Python Telegram Bot
+#Documentation: https://python-telegram-bot.readthedocs.io
+
+from datetime import datetime, timezone
+#import telegram
+import os
+
+#Fonction chargée de l'envoi de message à un utilisateur
+def envoyer_texte(update, texte_a_envoyer):
+	message = update.message
+
+	#Le texte à envoyer
+	message.reply_text(texte_a_envoyer)
+
+	#On créé une nouvelle entrée dans le journal de chat
+	bot_username = os.environ.get('TELEGRAM_BOT_USERNAME')
+	bot_id = os.environ.get('TELEGRAM_BOT_ID')
+	journaliser_message(datetime.now(),None,bot_username,bot_id,message.from_user.username,message.from_user.id,texte_a_envoyer)
+
+#Convertit une date/heure UTC en date/heure locale
+def date_utc_vers_local(date_utc):
+	date_locale = date_utc.replace(tzinfo=timezone.utc).astimezone(tz=None)
+	date_locale = date_locale.replace(tzinfo=None)
+	return date_locale.replace(microsecond=0)
+
+def journaliser_message_recu(update):
+	message = update.message
+
+	#On récupère toutes les infos nécessaire à la fonction "journaliser_message()"
+	date_envoi = message.date
+	date_reception = datetime.now()
+	expediteur = message.from_user.username
+	id_expediteur = message.from_user.id
+	destinataire = os.environ.get('TELEGRAM_BOT_USERNAME')
+	id_destinataire = os.environ.get('TELEGRAM_BOT_ID')
+	commandeRecue = message.text
+
+	#Puis on les envoient dans la fonction
+	journaliser_message(date_envoi, date_reception, expediteur, id_expediteur, destinataire, id_destinataire, commandeRecue)
+
+#Permet d'enregistrer chaque message reçu ou envoyé dans un fichier de log pour des raisons de contrôle/de surveillance
+def journaliser_message(date_envoi,date_reception,expediteur,id_expediteur,destinataire,id_destinataire,message):
+	nomFichierLOG = '/var/www/html/script/telegram/chat.log'
+
+	#On formate les date à la date/heure locale
+	date_envoi = date_utc_vers_local(date_envoi)
+	if date_reception != None:
+		date_reception = date_utc_vers_local(date_reception)
+
+	#On prépare la ligne qui sera enregistrée dans le fichier de log
+	entree_journal_log = '"%s","%s","%s","%s","%s","%s","%s"\n' % (date_envoi,date_reception,expediteur,id_expediteur,destinataire,id_destinataire,message)
+
+	#Si le fichier de log n'existe pas, on le créé et on ajoute les titres des colonnes
+	if os.path.exists(nomFichierLOG) == False:
+		with open(nomFichierLOG,'w') as fichierLOG:
+			fichierLOG.write('"Date d\'envoi","Date de réception","Expéditeur","ID expéditeur","Destinataire","ID destinataire","Message"\n')
+
+	#Écriture de l'entrée dans le journal
+	with open(nomFichierLOG,'a') as fichierLOG:
+		fichierLOG.write(entree_journal_log)
