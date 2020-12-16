2128b90886e69328eeb11daf36b0b6adcac8c04c
Natars-sourcecodes <pasero.romain@gmail.com>
2020-12-16 09:36:40 +0100

    Ce script est chargée de traiter les commandes reçues par le bot. Il utilise le script "message.py" en tant que module.
    DICTIONNAIRE DE COMMANDES:
    Le dictionnnaire "commandeActive" permet de faire l'association entre les commandes et les fonctions correspondantes.
    Les clés sont les noms de commandes (sans le "/"). Les valeurs associée sont les noms de fonctions correspondantes.
    
    Lorsqu'une commande du type "commandeActive[commande]()" est exécutée, la fonction associée est exécutée.
    i.e: "commandeActive['start'](update)" lancera la fonction "start()" avec la variable "update" en tant qu'argument.
    
    Ce dictionnaire est utilisé par la fonction "traiter_commande()" afin de lancer la fonction correspondante à la commande reçue.
    
    DETAIL DES FONCTIONS DU SCRIPT:
    La fonction principale est "traiter_commande()": c'est, en quelques sortes, la porte d'entrée des nouvelles commandes reçues.
    Lorsqu'une commande est reçu, le script "main.py" appelle cette fonction en lui transmettant le "update" (contient toutes les infos sur la dernière commande reçue).
    Cette fonction se charge, en premier lieu, de journaliser la commande reçue.
    Puis, elle supprime le "/" de la commande et essaie de lancer la fonction dédiée (dans le dictionnaire "commandeActive").
    Un booléen permet d'indiquer le succès du lancement ou non.
    
    Les fonctions portant le nom de commandes sont exécutées dès que la commande correspondante est reçue.
     - start: active le bot (édition de la variable d'environnement "TELEGRAM_BOT_ACTIF à True) et confirmation d'action dans le chat.
     - stop: désactive le bot (édition de la variable d'environnement "TELEGRAM_BOT_ACTIF à False) et confirmation d'action dans le chat.
     - statut: intique l'état du bot (lecture de la variable d'environnement "TELEGRAM_BOT_ACTIF) via un message dans le chat.
    
    La fonction "get_commande_active()" est chargée de renvoyer la listes des clés du dictionnaire "commandeActive".
    Il est principalement utilisé pour connaître la liste des commandes activées (notamment pour indiquer au bot quelle commande surveiller).

---
+++ script/telegram/commande.py
@@ -0,0 +1,53 @@
+#Script contenant l'ensemble des commandes que le bot sait traiter
+
+import message
+import os
+
+def start(update):
+	os.environ['TELEGRAM_BOT_ACTIF'] = 'True'
+	message.envoyer_texte(update, "Bot activé")
+
+def stop(update):
+	os.environ['TELEGRAM_BOT_ACTIF'] = 'False'
+	message.envoyer_texte(update, "Bot désactivé")
+
+def statut(update):
+	if os.environ.get('TELEGRAM_BOT_ACTIF') == 'True':
+		message_a_envoyer = "Bot actif"
+	else:
+		message_a_envoyer = "Bot inactif"
+
+	message.envoyer_texte(update, message_a_envoyer)
+
+#On créé un dictionnaire dans lequel sont listées les commandes actives
+#Elles sont associé à la fonction correspondantes définie ci-dessus
+commandeActive = {}
+commandeActive['start'] = start
+commandeActive['stop'] = stop
+commandeActive['statut'] = statut
+
+#Fonction principal, permet de traiter une commande reçue par le bot
+def traiter_commande(update):
+	#On commence par journaliser le nouveau message reçu sur le chat
+	message.journaliser_message_recu(update)
+
+	#On récupère la commande et lui supprime son '/'
+	commandeRecue = update.message.text
+	commandeRecue = commandeRecue[1:]
+
+	#On essaie de lancer la fonction correspondante
+	#On renvoie le resultat via un booléen:
+	#
+	#	True => ça fonctionne à merveille
+	#	False => commande inconnue/désactivée
+	#
+	try:
+		commandeActive[commandeRecue](update)
+		return True
+
+	except KeyError:
+		return False
+
+#Renvoie la liste des commandes actives
+def get_commande_active():
+	return commandeActive.keys()
